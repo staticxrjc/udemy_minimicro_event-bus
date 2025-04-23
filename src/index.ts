282f332935b53d5801b59c67a3eebfa5fc55e18d
staticxrjc <staticxrjc@gmail.com>
2025-04-22 17:19:10 -0700

    Updated events with new event bus

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Happy developing ✨')
+import express, {Express} from 'express';
+import bodyParser from 'body-parser';
+import axios from "axios";
+import cors from 'cors';
+
+const app : Express = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/events', (req, res) => {
+    const event = req.body;
+
+    axios.post('http://localhost:4000/events', event);
+    axios.post('http://localhost:4001/events', event);
+    axios.post('http://localhost:4002/events', event);
+
+    res.send({status: 'OK'});
+});
+
+app.listen(4005, () => {
+    console.log('Listening on 4005');
+});
